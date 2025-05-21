912dac514320f2c67f8344970155cca398224b0e
Dasith Dilina Malinda <dasithdilinamalinda@gmail.com>
2025-05-21 23:41:07 +0530

    initial commit: add HTML, CSS, and JavaScript files for PWD-HTML project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log('js is running')
+console.log(navigator.serviceWorker)
+// check have serviceWorker
+if('serviceWorker' in navigator){
+    console.log("serviceWorker is working here")
+}
